667e7f441bbdc6091f1b0a9d092b802ec5bafe83
candy4390 <2459402883@qq.com>
2019-12-27 17:20:43 +0800

    图

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { ObserverPattern } from './patterns/observer-pattern';
 import { BinarySearchTree } from './patterns/binary-search-tree';
+import { Picture } from './patterns/picture';
+import { Stack } from './patterns/stack';
 
 // const observer = new ObserverPattern();
 // function callback() {
@@ -14,24 +16,68 @@ import { BinarySearchTree } from './patterns/binary-search-tree';
 // observer.emit('complate', 'dsn');
 // observer.off('complate', callback);
 
-const bt = new BinarySearchTree();
-bt.insert(11);
-bt.insert(7);
-bt.insert(15);
-bt.insert(5);
-bt.insert(3);
-bt.insert(9);
-bt.insert(8);
-bt.insert(10);
-bt.insert(13);
-bt.insert(12);
-bt.insert(14);
-bt.insert(20);
-bt.insert(18);
-bt.insert(25);
-bt.insert(6);
-function print(value) {
-    console.log(value);
-}
-bt.inOrderTraverse(print);
-console.log('-------');
+// const bt = new BinarySearchTree();
+// bt.insert(11);
+// bt.insert(7);
+// bt.insert(15);
+// bt.insert(5);
+// bt.insert(3);
+// bt.insert(9);
+// bt.insert(8);
+// bt.insert(10);
+// bt.insert(13);
+// bt.insert(12);
+// bt.insert(14);
+// bt.insert(20);
+// bt.insert(18);
+// bt.insert(25);
+// bt.insert(6);
+// function print(value) {
+//     console.log(value);
+// }
+// bt.inOrderTraverse(print);
+// console.log('-------');
+
+const graph = new Picture();
+const myVertices = ['A','B','C','D','E','F','G','H','I'];
+myVertices.forEach(vertice => {
+    graph.addVertex(vertice);
+});
+graph.addEdge('A', 'B'); //{9}
+graph.addEdge('A', 'C');
+graph.addEdge('A', 'D');
+graph.addEdge('B', 'A');
+graph.addEdge('B', 'E');
+graph.addEdge('B', 'F');
+graph.addEdge('E', 'B');
+graph.addEdge('E', 'I');
+graph.addEdge('I', 'E');
+graph.addEdge('F', 'B');
+graph.addEdge('C', 'A');
+graph.addEdge('C', 'D');
+graph.addEdge('C', 'G');
+graph.addEdge('D', 'A');
+graph.addEdge('D', 'G');
+graph.addEdge('D', 'H');
+graph.addEdge('G', 'C');
+graph.addEdge('G', 'D');
+graph.addEdge('H', 'D');
+// const shortestPath = graph.bfs('A');
+// const fromVertex = myVertices[0];
+// myVertices.forEach(vertice => {
+//     const toVertex = vertice;
+//     const path = new Stack();
+//     for (let v = toVertex; v !== fromVertex; v = shortestPath.predecessors[v]) {
+//         path.push(v);
+//     }
+//     path.push(fromVertex);
+//     let s = path.pop();
+//     while(!path.isEmpty()) {
+//         s += ' - ' + path.pop();
+//     }
+//     console.log(s);
+// });
+graph.dfs((v) => {
+    console.log('visited: ' + v);
+})
+
--- src/patterns/picture.ts
+++ src/patterns/picture.ts
@@ -1,12 +1,153 @@
+import { Dictionary } from "./dictionary";
+import { Queue } from "./queue";
 
 /**
- * 数据结构图
+ * 数据结构之---图
+ * 本实例使用邻接表来实现图的结构
  *
  * @export
  * @class Picture
  */
 export class Picture {
+    private vertices = []; // 存储顶点的名称
+    private adjList = new Dictionary(); // 存储顶点对应的邻接表
     constructor() {
-        
+
+    }
+
+    /**
+     * 向图中添加新的顶点
+     *
+     * @param {string} v
+     * @memberof Picture
+     */
+    addVertex(v: string) {
+        this.vertices.push(v);
+        this.adjList.set(v, []);
+    }
+    /**
+     * 添加顶点之间的边
+     *
+     * @param {string} v
+     * @param {string[]} w
+     * @memberof Picture
+     */
+    addEdge(v: string, w: string) {
+        this.adjList.get(v).push(w);
+    }
+    toString() {
+        let s = '';
+        this.vertices.forEach(vertice => {
+            s += vertice + ' -> ';
+            const neighbors = this.adjList.get(vertice);
+            neighbors.forEach(neighbor => {
+                s += neighbor + ' ';
+            });
+            s += '\n';
+        });
+        return s;
+    }
+
+    /**
+     * 白色:表示该顶点还没有被访问。
+     * 灰色:表示该顶点被访问过，但并未被探索过。
+     * 黑色:表示该顶点被访问过且被完全探索过。
+     *
+     * @returns
+     * @memberof Picture
+     */
+    initializeColor() {
+        const color = [];
+        this.vertices.forEach(vertice => {
+            color[vertice] = 'white';
+        });
+        return color;
+    }
+
+    /**
+     * 广度优先搜索
+     *
+     * @param {*} v 图的顶点
+     * @param {*} callback 回调函数
+     * @memberof Picture
+     */
+    // bfs(v: string, callback: Function) {
+    //     const color = this.initializeColor();
+    //     const queue = new Queue();
+    //     queue.enqueue(v);
+    //     while(!queue.isEmpty()) {
+    //         const u = queue.dequeue();
+    //         const neighbors = this.adjList.get(u); // 取到u相邻的顶点列表
+    //         color[u] = 'grey';
+    //         neighbors.forEach((neighbor: string) => {
+    //             if (color[neighbor] === 'white') {
+    //                 color[neighbor] = 'grey';
+    //                 queue.enqueue(neighbor);
+    //             }
+    //         });
+    //         color[u] = 'black';
+    //         if (callback) {
+    //             callback(u);
+    //         }
+    //     }
+    // }
+
+    bfs(v: string) {
+        const color = this.initializeColor();
+        const queue = new Queue();
+        const d = [];
+        const pred = [];
+        queue.enqueue(v);
+        this.vertices.forEach(vertice => {
+            d[vertice] = 0;
+            pred[vertice] = null;
+        });
+        while(!queue.isEmpty()) {
+            const u = queue.dequeue();
+            const neighbors = this.adjList.get(u);
+            color[u] = 'grey';
+            neighbors.forEach(neighbor => {
+                if (color[neighbor] === 'white') {
+                    color[neighbor] = 'grey';
+                    d[neighbor] = d[u] + 1;
+                    pred[neighbor] = u;
+                    queue.enqueue(neighbor);
+                }
+            });
+            color[u] = 'black';
+        }
+        return {
+            distance: d,
+            predecessors: pred
+        }
+    }
+
+    /**
+     * 深度优先搜索
+     *
+     * @param {Function} callback
+     * @memberof Picture
+     */
+    dfs(callback: Function) {
+        const color = this.initializeColor();
+        this.vertices.forEach(vertice => {
+            if (color[vertice] === 'white') {
+                this.dfsVisit(vertice, color, callback);
+            }
+        });
+    }
+
+    dfsVisit(u: string, color: any, callback: Function) {
+        color[u] = 'grey';
+        if (callback) {
+            callback(u);
+        }
+        const neighbors = this.adjList.get(u);
+        neighbors.forEach(neighbor => {
+            if (color[neighbor] === 'white') {
+                this.dfsVisit(neighbor, color, callback);
+            }
+        });
+        color[u] = 'black';
     }
 }
