79136032a35597a04c33e5922bf78045aa52decc
陈小祥 <xiaoxiang930601@163.com>
2021-12-27 09:23:28 +0800

    节流+数组扁平

---
+++ src/es6/flat-array.ts
@@ -0,0 +1,44 @@
+
+/**
+ * 数组扁平化---递归
+ *
+ * @param {*} arr
+ * @return {*} 
+ */
+function flatten(arr) {
+    let result = [];
+    arr.forEach(item => {
+        if (item instanceof Array) {
+            result.push(...flatten(item))
+        } else {
+            result.push(item);
+        }
+    })
+    return result;
+}
+
+// 如果元素都是数字或字符串
+[1, [2, [3, 4]]].toString() // "1,2,3,4"
+
+/**
+ * 数组扁平化---使用reduce+递归
+ *
+ * @param {*} arr
+ */
+function flatten2(arr) {
+    return arr.reduce((prev, next) => {
+        return prev.concat(Array.isArray(next) ? flatten2(next) : next)
+    })
+}
+
+/**
+ * 数组扁平化---使用扩展运算符(只可以扁平一层)+some
+ *
+ * @param {*} arr
+ */
+function flatten3(arr) {
+    while (arr.some(i => Array.isArray(i))) {
+        arr = [].concat(...arr);
+    }
+}
+
--- src/es6/my-throttle.ts
+++ src/es6/my-throttle.ts
@@ -1,13 +1,14 @@
 
 /**
- * 节流函数
+ * 节流函数---使用定时器
  * 连续触发事件但是在 n 秒中只执行一次函数。节流会稀释函数的执行频率。
+ * 停止触发后，还在执行一下（setTimeout还没执行完）
  *
  */
 function MyThrottle(fn: Function, interval: number) {
     let flag = null;
-    let that = this;
     return function() {
+        let that = this;
         if (!flag) {
             flag = true;
             const args = arguments;
@@ -16,6 +17,26 @@ function MyThrottle(fn: Function, interval: number) {
                 fn.apply(that, args);
                 clearTimeout(t$);
             }, interval);
+        }
+    }
+}
+
+/**
+ * 节流---使用时间戳,如果希望触发时立即执行，可以将previous初始值设为0
+ * 停止触发后不会再次执行
+ *
+ * @param {*} fn
+ * @param {*} delay
+ */
+function MyThrottle2(fn: Function, delay: number) {
+    let previous = new Date().getTime();
+    return function() {
+        const context = this;
+        const args = arguments;
+        let now = new Date().getTime();
+        if (now - previous > delay) {
+            fn.apply(context, args);
+            previous = now;
         }
     }
 }
